b12e294bfab3651f1870d543b87e5a27b9d7282a
Melkia <bonfil99melkia@gmail.com>
2020-06-29 16:13:25 -0500

    theme selector

--- js/index.js
+++ js/index.js
@@ -47,6 +47,19 @@ $(document).ready(function(){
         $('.publicaciones').append(post);
     })
 
-
+    /** Theme selector **/
     
+    const theme = $('#theme-color');
+
+    $('.to-green').click(function(){
+        theme.attr('href','../css/green.css')
+    })
+
+    $('.to-blue').click(function(){
+        theme.attr('href','../css/blue.css')
+    })
+
+    $('.to-red').click(function(){
+        theme.attr('href','../css/red.css')
+    })
 });
